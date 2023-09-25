fdd261ec32136bbab50c42693de1159d61664c38
Rico Ulul Ilmy <rico.ui@bki.co.id>
2023-09-25 03:23:50 +0000

    Update gitlabpull.sh

--- gitlabpull.sh
+++ gitlabpull.sh
@@ -116,11 +116,11 @@ elif  [ "stagingcontainer" = $CI_JOB_STAGE ]; then
     docker exec tkdn-bki-php id www-data
     docker exec tkdn-bki-php ls -l /var/www/html/storage
     
-	  docker exec tkdn-bki-php composer install --ignore-platform-reqs
+	docker exec tkdn-bki-php composer install --ignore-platform-reqs
     docker exec tkdn-bki-php php artisan key:generate
     docker exec tkdn-bki-php php artisan storage:link
-    #docker exec tkdn-bki-php php artisan migrate
-    #docker exec tkdn-bki-php php artisan db:seed
+    docker exec tkdn-bki-php php artisan migrate
+    docker exec tkdn-bki-php php artisan db:seed
 
     # cd db
     # echo "\n import last database"
