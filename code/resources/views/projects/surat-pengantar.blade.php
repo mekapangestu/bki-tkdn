cac260e508d9ea412f0e5327b49eca0106a85471
Muhamad Abdi Mulia <puntangwine@gmail.com>
2023-09-25 07:10:49 +0700

    fix title

--- code/resources/views/projects/surat-pengantar.blade.php
+++ code/resources/views/projects/surat-pengantar.blade.php
@@ -29,7 +29,7 @@
 
             <!-- PAGE-HEADER -->
             <div class="page-header">
-                <h1 class="page-title">Input TKDN</h1>
+                <h1 class="page-title">Upload Surat</h1>
                 <div>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
--- code/resources/views/projects/verify2.blade.php
+++ code/resources/views/projects/verify2.blade.php
@@ -29,7 +29,7 @@
 
             <!-- PAGE-HEADER -->
             <div class="page-header">
-                <h1 class="page-title">Pilih Asesor & QC</h1>
+                <h1 class="page-title">Verifikasi</h1>
                 <div>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
