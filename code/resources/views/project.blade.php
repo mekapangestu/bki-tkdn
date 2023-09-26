d952910d8d5b8a243e6a893cc466a8b475a1fb44
Muhamad Abdi Mulia <puntangwine@gmail.com>
2023-09-27 00:26:17 +0700

    fix

--- code/resources/views/project.blade.php
+++ code/resources/views/project.blade.php
@@ -107,9 +107,11 @@
                                                         <li><a href="{{ route('projects.submit', [$item->id, 4]) }}" class="btn text-warning btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View"><span class="fe fe-eye fs-14"></span> Freeze, Tak Lengkap</a></li>
                                                     @elseif($item->stage == 1)
                                                         <li><a href="{{route('projects.show', $item->id)}}" class="btn text-primary btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View"><span class="fe fe-eye fs-14"></span> Pilih Asesor & QC</a></li>
-                                                        @endif
+                                                    @endif
                                                         <li><a href="{{route('projects.detail', $item->id)}}" class="btn text-primary btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View"><span class="fe fe-eye fs-14"></span> Detail</a></li>
-
+                                                    @if ($item->url_sertifikat_terbit)
+                                                        <li><a href="{{url($item->url_sertifikat_terbit)}}" class="btn text-primary btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View" target="_blank"><span class="fe fe-eye fs-14"></span> View Sertifikat</a></li>
+                                                    @endif
                                                 </ul>
                                             </div>
                                             @endrole
@@ -118,6 +120,11 @@
                                                 <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="dropdownMenu" data-bs-toggle="dropdown" aria-expanded="false">
                                                     <span class="fe fe-more-horizontal fs-14"></span>
                                                 </button>
+                                                @if ($item->url_sertifikat_terbit)
+                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu">
+                                                        <li><a href="{{url($item->url_sertifikat_terbit)}}" class="btn text-primary btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View" target="_blank"><span class="fe fe-eye fs-14"></span> View Sertifikat</a></li>
+                                                    </ul>
+                                                @endif
 
                                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu">
                                                     <li><a href="{{route('projects.edit', $item->id)}}" class="btn text-primary btn-sm" data-bs-toggle="tooltip" data-bs-original-title="View"><span class="fe fe-eye fs-14"></span> Lengkapi Data</a></li>
