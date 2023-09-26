465da0b88a6eba14967d0d31cb71d2de155e47a1
Muhamad Eka Pangestu <mekapangestu@gmail.com>
2023-09-26 22:53:13 +0700

    Add dashboard recap

--- code/resources/views/dashboard.blade.php
+++ code/resources/views/dashboard.blade.php
@@ -1,27 +1,33 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
-<style>
-    .leaflet-sidebar{
-       bottom: 30px!important; 
-    }
-    .leaflet-bottom.leaflet-right{
-        position: absolute!important;
-        right: 0!important;
-    }
-    .context table { width: inherit !important; }
-    .context table td, 
-    .context table th{
-        white-space: nowrap;
-        width: 1%;
-        padding: 6px;
-        margin: 0;
-    }
-    .leaflet-sidebar:not(.collapsed) {
-        width: 50%!important;
-        max-width: 50%!important;
-    }
-</style>
+    <style>
+        .leaflet-sidebar {
+            bottom: 30px !important;
+        }
+
+        .leaflet-bottom.leaflet-right {
+            position: absolute !important;
+            right: 0 !important;
+        }
+
+        .context table {
+            width: inherit !important;
+        }
+
+        .context table td,
+        .context table th {
+            white-space: nowrap;
+            width: 1%;
+            padding: 6px;
+            margin: 0;
+        }
+
+        .leaflet-sidebar:not(.collapsed) {
+            width: 50% !important;
+            max-width: 50% !important;
+        }
+    </style>
     <div class="side-app">
 
         <!-- CONTAINER -->
@@ -51,28 +57,80 @@
             @endif
             <div class="row">
                 <div class="col-12">
-                     <!-- ROW-1 -->
-                     <div class="row">
+                    <!-- ROW-1 -->
+                    <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
+                            <h5>Rekapitulasi Permohonan Perhitungan TKDN</h5>
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
                                     <div class="card overflow-hidden">
                                         <div class="card-body">
                                             <div class="d-flex">
                                                 <div class="mt-2">
+                                                    <h6 class="">Total Permohonan</h6>
+                                                    <h2 class="mb-0 number-font">{{ $projects }}</h2>
+                                                </div>
+                                                <div class="ms-auto">
+                                                    <div class="chart-wrapper mt-1">
+                                                        <canvas id="saleschart" class="h-8 w-9 chart-dropshadow"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                                    <div class="card overflow-hidden">
+                                        <div class="card-body">
+                                            <div class="d-flex">
+                                                <div class="mt-2">
+                                                    <h6 class="">Total Produk</h6>
+                                                    <h2 class="mb-0 number-font">{{ $orders }}</h2>
+                                                </div>
+                                                <div class="ms-auto">
+                                                    <div class="chart-wrapper mt-1">
+                                                        <canvas id="leadschart" class="h-8 w-9 chart-dropshadow"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                                    <div class="card overflow-hidden">
+                                        <div class="card-body">
+                                            <div class="d-flex">
+                                                <div class="mt-2">
                                                     <h6 class="">Total Users</h6>
-                                                    <h2 class="mb-0 number-font">44,278</h2>
+                                                    <h2 class="mb-0 number-font">{{ $users }}</h2>
                                                 </div>
                                                 <div class="ms-auto">
                                                     <div class="chart-wrapper mt-1">
-                                                        <canvas id="saleschart"
-                                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                                        <canvas id="profitchart" class="h-8 w-9 chart-dropshadow"></canvas>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <span class="text-muted fs-12"><span class="text-secondary"><i
-                                                        class="fe fe-arrow-up-circle  text-secondary"></i> 5%</span>
-                                                Last week</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <h5>Rekapitulasi Status Permohonan</h5>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xl-3">
+                                    <div class="card overflow-hidden">
+                                        <div class="card-body">
+                                            <div class="d-flex">
+                                                <div class="mt-2">
+                                                    <h6 class="">Total Permohonan Diterima</h6>
+                                                    <h2 class="mb-0 number-font">{{ $accept }}</h2>
+                                                </div>
+                                                <div class="ms-auto">
+                                                    <div class="chart-wrapper mt-1">
+                                                        <canvas id="saleschart" class="h-8 w-9 chart-dropshadow"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -81,19 +139,15 @@
                                         <div class="card-body">
                                             <div class="d-flex">
                                                 <div class="mt-2">
-                                                    <h6 class="">Total Profit</h6>
-                                                    <h2 class="mb-0 number-font">67,987</h2>
+                                                    <h6 class="">Total Permohonan Ditolak</h6>
+                                                    <h2 class="mb-0 number-font">{{ $denied }}</h2>
                                                 </div>
                                                 <div class="ms-auto">
                                                     <div class="chart-wrapper mt-1">
-                                                        <canvas id="leadschart"
-                                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                                        <canvas id="leadschart" class="h-8 w-9 chart-dropshadow"></canvas>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <span class="text-muted fs-12"><span class="text-pink"><i
-                                                        class="fe fe-arrow-down-circle text-pink"></i> 0.75%</span>
-                                                Last 6 days</span>
                                         </div>
                                     </div>
                                 </div>
@@ -102,19 +156,15 @@
                                         <div class="card-body">
                                             <div class="d-flex">
                                                 <div class="mt-2">
-                                                    <h6 class="">Total Expenses</h6>
-                                                    <h2 class="mb-0 number-font">$76,965</h2>
+                                                    <h6 class="">Total Permohonan Diterima Tidak Lengkap</h6>
+                                                    <h2 class="mb-0 number-font">{{ $acceptstatus }}</h2>
                                                 </div>
                                                 <div class="ms-auto">
                                                     <div class="chart-wrapper mt-1">
-                                                        <canvas id="profitchart"
-                                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                                        <canvas id="profitchart" class="h-8 w-9 chart-dropshadow"></canvas>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <span class="text-muted fs-12"><span class="text-green"><i
-                                                        class="fe fe-arrow-up-circle text-green"></i> 0.9%</span>
-                                                Last 9 days</span>
                                         </div>
                                     </div>
                                 </div>
@@ -123,19 +173,15 @@
                                         <div class="card-body">
                                             <div class="d-flex">
                                                 <div class="mt-2">
-                                                    <h6 class="">Total Cost</h6>
-                                                    <h2 class="mb-0 number-font">$59,765</h2>
+                                                    <h6 class="">Total Permohonan Freeze</h6>
+                                                    <h2 class="mb-0 number-font">{{ $freeze }}</h2>
                                                 </div>
                                                 <div class="ms-auto">
                                                     <div class="chart-wrapper mt-1">
-                                                        <canvas id="costchart"
-                                                            class="h-8 w-9 chart-dropshadow"></canvas>
+                                                        <canvas id="profitchart" class="h-8 w-9 chart-dropshadow"></canvas>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <span class="text-muted fs-12"><span class="text-warning"><i
-                                                        class="fe fe-arrow-up-circle text-warning"></i> 0.6%</span>
-                                                Last year</span>
                                         </div>
                                     </div>
                                 </div>
@@ -145,11 +191,11 @@
                     <!-- ROW-1 END -->
                 </div>
             </div>
-        <!-- CONTAINER END -->
-    </div>
-@endsection
-@section('js')
-<script src="{{asset('assets/plugins/leaflet/leaflet.js')}}"></script>
-<script src='https://cdn.jsdelivr.net/npm/leaflet-sidebar-v2@3.1.1/js/leaflet-sidebar.min.js'></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-sidebar-v2@3.1.1/css/leaflet-sidebar.min.css">
-@endsection
+            <!-- CONTAINER END -->
+        </div>
+    @endsection
+    @section('js')
+        <script src="{{ asset('assets/plugins/leaflet/leaflet.js') }}"></script>
+        <script src='https://cdn.jsdelivr.net/npm/leaflet-sidebar-v2@3.1.1/js/leaflet-sidebar.min.js'></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-sidebar-v2@3.1.1/css/leaflet-sidebar.min.css">
+    @endsection
