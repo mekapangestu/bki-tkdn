7edcbc0bfd98f6729f9bc316dcc949ddfee0a2d0
Muhamad Eka Pangestu <mekapangestu@gmail.com>
2023-10-03 11:47:16 +0700

    Update landing and detail page

--- code/resources/views/emails/welcome.blade.php
+++ code/resources/views/emails/welcome.blade.php
@@ -1,18 +1,129 @@
-Hey {{$name}}, Welcome to our website. <br>
-<table>
-    <tr>
-        <td>Nama</td>
-        <td>:</td>
-        <td>{{$name}}</td>
-    </tr>
-    <tr>
-        <td>Email</td>
-        <td></td>
-        <td>{{$email}}</td>
-    </tr>
-    <tr>
-        <td>Password</td>
-        <td></td>
-        <td>{{$password}}</td>
-    </tr>
+<p>Selamat Datang&nbsp;<strong>{{ $name }}</strong> di aplikasi <strong>TKDN PT. Biro Klasifikasi Indonesia.&nbsp;</strong>Permohonan anda sudah kami terima.<br>
+    Anda sudah kami buatkan akun untuk dapat melanjutkan proses verifikasi perhitungan nilai TKDN anda pada aplikasi&nbsp;kami.</p>
+
+<br>
+
+<p>Berikut detail akun anda :&nbsp;</p>
+
+<table align="left" border="0" cellpadding="1" cellspacing="1" style="width:382px">
+    <tbody>
+        <tr>
+            <td style="width:99px">Nama Lengkap</td>
+            <td style="width:12px">:</td>
+            <td style="width:252px"><strong>{{ $name }}</strong></td>
+        </tr>
+        <tr>
+            <td style="width:99px">Email</td>
+            <td style="width:12px">:</td>
+            <td style="width:252px"><strong>{{ $email }}</strong></td>
+        </tr>
+        <tr>
+            <td style="width:99px">Password</td>
+            <td style="width:12px">:</td>
+            <td style="width:252px"><strong>{{ $password }}</strong></td>
+        </tr>
+    </tbody>
+</table> <br><br>
+
+<p>Klik&nbsp;<a href="http://127.0.0.1:8000">disini</a>&nbsp;untuk melanjutkan login.</p>
+
+<br>
+
+<p><strong>*Mohon&nbsp;untuk melengkapi dokumen-dokumen yang dibutuhkan untuk perhitungan nilai TKDN produk anda.</strong></p>
+
+<p><strong>Berikut daftar dokumen yang harus di upload </strong>:</p>
+
+<p><strong>A.&nbsp;Lingkup Kebutuhan Dokumen TKDN Barang</strong></p>
+
+<table align="left" border="0" cellpadding="1" cellspacing="1" style="width:500px">
+    <tbody>
+        <tr>
+            <td><strong>Aspek Produksi</strong></td>
+            <td><strong>Aspek Pemasaran</strong></td>
+        </tr>
+        <tr>
+            <td>
+                <ol>
+                    <li>Flow Proses Produksi</li>
+                    <li>Laporan Produksi</li>
+                    <li>Bill off Material/ Komponen Produksi</li>
+                    <li>Nilai Perolehan Material Produksi</li>
+                    <li>Photo komponen material produksi</li>
+                    <li>Daftar dan Nilai Perolehan Mesin Produksi</li>
+                    <li>Struktur Organisasi Produksi</li>
+                </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Katalog Produk</li>
+                    <li>Laporan Penjualan</li>
+                    <li>Jalur Pemasaran</li>
+                </ol>
+            </td>
+        </tr>
+    </tbody>
+</table> <br><br<br>
+
+<p><strong>B.&nbsp;Lingkup Kebutuhan Dokumen TKDN Barang &amp; Jasa</strong></p>
+
+<table align="left" border="0" cellpadding="1" cellspacing="1" style="width:100%">
+    <tbody>
+        <tr>
+            <td>
+                <h4><strong>Kontrak</strong></h4>
+            </td>
+            <td>
+                <h4><strong>Material</strong></h4>
+            </td>
+            <td>
+                <h4><strong>Tenaga Kerja</strong></h4>
+            </td>
+            <td>
+                <h4><strong>Alat Kerja</strong></h4>
+            </td>
+            <td>
+                <h4><strong>Jasa Umum</strong></h4>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <ol>
+                    <li>RAB, BOQ, AHSP</li>
+                    <li>Scope Of Work</li>
+                    <li>Profil Proyek</li>
+                    <li>Daftar Vendor/Subkontraktor</li>
+                    <li>Foto Proyek</li>
+                </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Invoice Pembelian Material</li>
+                    <li>Sertifikat TKDN</li>
+                    <li>Mill Certificate</li>
+                </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Struktur Organisasi Proyek</li>
+                    <li>Daftar Tenaga Kerja</li>
+                    <li>Bukti Kewarganegaraan Tenaga Kerja</li>
+                </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Bukti Kepemilikan Alat Kerja</li>
+                    <li>Bukti Penyewaan Alat Kerja (jika sewa)</li>
+                    <li>Bukti Status Kepemilikan Saham Perusahaan</li>
+                </ol>
+            </td>
+            <td>
+                <ol>
+                    <li>Bukti pembayaran jasa (Mobilisasi, dll)</li>
+                    <li>Perhitungan TKDN Vendor/Subkontraktor</li>
+                </ol>
+            </td>
+        </tr>
+    </tbody>
 </table>
+
+<p>&nbsp;</p>
