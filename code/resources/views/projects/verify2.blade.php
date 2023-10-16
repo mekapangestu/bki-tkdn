ca535d7ddb30aa58bd4405a84bc550ec1d6093e3
Muhamad Abdi Mulia <puntangwine@gmail.com>
2023-10-16 17:30:20 +0700

    fix

--- code/resources/views/projects/verify2.blade.php
+++ code/resources/views/projects/verify2.blade.php
@@ -363,25 +363,25 @@
                                             @endforeach
                                         </tbody>
                                     </table>
-                                    <div class="card custom-card">
-                                        <div class="card-header border-bottom">
-                                            <h3 class="card-title">Foto Produk</h3>
-                                        </div>
-                                        <div class="card-body">
-                                            <ul class="lightgallery list-unstyled row">
-                                                @foreach ($project->foto as $file)
-                                                    @if (Str::is(Str::headline($item->produk) . '*', $file->label))
-                                                        <li class="col-xs-6 col-sm-4 col-md-4 col-xl-3 mb-5 border-bottom-0" data-responsive="{{ asset('storage/' . $file->path) }}" data-src="{{ asset('storage/' . $file->path) }}" data-sub-html="<h4>{{Str::headline($item->produk)}}</h4>">
-                                                            <a href="javascript:void(0)">
-                                                                <img class="img-responsive br-5" src="{{ asset('storage/' . $file->path) }}">
-                                                            </a>
-                                                        </li>
-                                                    @endif
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="card custom-card">
+                            <div class="card-header border-bottom">
+                                <h3 class="card-title">Foto Produk</h3>
+                            </div>
+                            <div class="card-body">
+                                <ul class="lightgallery list-unstyled row">
+                                    @foreach ($project->foto as $file)
+                                        @if (Str::is(Str::headline($item->produk) . '*', $file->label))
+                                            <li class="col-xs-6 col-sm-4 col-md-4 col-xl-3 mb-5 border-bottom-0" data-responsive="{{ asset('storage/' . $file->path) }}" data-src="{{ asset('storage/' . $file->path) }}" data-sub-html="<h4>{{Str::headline($item->produk)}}</h4>">
+                                                <a href="javascript:void(0)">
+                                                    <img class="img-responsive br-5" src="{{ asset('storage/' . $file->path) }}">
+                                                </a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
                             </div>
                         </div>
                     @empty
