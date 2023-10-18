c8529166512504a8504afcd352d50ca140d9228c
Muhamad Abdi Mulia <puntangwine@gmail.com>
2023-10-18 21:48:49 +0700

    Revert "add additional"
    This reverts commit 738f784f0708089c4ec0871c7be49c1a7719d9d1.

--- code/app/Http/Controllers/RequestController.php
+++ code/app/Http/Controllers/RequestController.php
@@ -339,9 +339,6 @@ class RequestController extends Controller
                     $additional->kd_kelompok_barang = $request->kd_kelompok_barang[$key];
                     $additional->merk = $request->merk[$key];
                     $additional->tipe = $request->tipe[$key];
-                    $additional->kd_hs = $request->kd_hs[$key];
-                    $additional->spesifikasi = $request->spesifikasi[$key];
-                    $additional->kbli = $request->kbli[$key];
                     $additional->save();
                 }
             }
