96276bce9a576e1d33cc17d79d35376fcbcf1e3f
Muhamad Abdi Mulia <puntangwine@gmail.com>
2023-11-18 11:28:59 +0700

    disable migration dev

--- gitlabpull.sh
+++ gitlabpull.sh
@@ -128,7 +128,7 @@ elif  [ "stagingcontainer" = $CI_JOB_STAGE ]; then
     docker exec tkdn-bki-php php artisan cache:clear
     docker exec tkdn-bki-php php artisan config:clear
     docker exec tkdn-bki-php php artisan storage:link
-    docker exec tkdn-bki-php php artisan migrate
+    # docker exec tkdn-bki-php php artisan migrate
     # docker exec tkdn-bki-php php artisan db:seed
 
     # cd db
